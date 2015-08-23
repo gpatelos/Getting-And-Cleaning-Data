6f078cf6e9c82ca148603651cb57fec0d2ad178b
gpatelos <greg.patselas@gmail.com>
2015-08-22 21:30:03 -0400

    run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -6,7 +6,6 @@
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 
-library("plyr")
 library("dplyr")
 library("data.table")
 library("reshape2")
@@ -75,8 +74,4 @@ tidy = aggregate(trimData2, by=list(Activity = trimData2$Activity, Subject=trimD
 
 tidy<-tidy[order(tidy$Subject,tidy$Activity),]
 write.table(tidy, file = "tidydata.txt",row.name=FALSE)
-
-#ids = c("Subject", "Activity", "ActivityID")
-#measurevars = setdiff(colnames(trimData2), ids)
-#trimData2melt <- melt(trimData2, id= ids, measure.vars = measurevars)
 
